d4ea05610b87c7578fe0cff0834ca434474916ec
Minoo Hong <mhong07@iastate.edu>
2026-04-09 13:25:55 -0500

    lab22

--- lecture22/job_pi.sh
+++ lecture22/job_pi.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=pi_compare
-#SBATCH --output=pi_compare_%j.out
-#SBATCH --error=pi_compare_%j.err
-#SBATCH --time=00:05:00
+
+# Copy/paste this job script into a text file and submit with the command:
+#    sbatch thefilename
+
+#SBATCH --partition=instruction
 #SBATCH --nodes=1
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=0-0:5:0
+#SBATCH --mem=4G
+#SBATCH --account=s2026.coms.5250.01
+#SBATCH --qos=instruction
+#SBATCH --job-name="lab22"
+#SBATCH --mail-user=mhong07@iastate.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output="pi_compare_%j.out"
+#SBATCH --error="pi_compare_%j.err"
 
 module purge
 module load openmpi/5.0.8-mybw4ys
